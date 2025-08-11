5ce94d0e52ce64d3a7378f33da6105fbd1c56ba5
khanhkieu <kieuxuankhanh12@gmail.com>
2025-08-11 17:06:54 +0700

    update codebase

--- src/main/java/com/example/demo/model/Story.java
+++ src/main/java/com/example/demo/model/Story.java
@@ -1,12 +1,16 @@
 package com.example.demo.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@Document(collection = "stories")
 public class Story {
+    @Id
     private UUID id;
     private String title;
     private String author;
